a21ec9d6cd04eafe6fc9f266894fe32b1facaf0a
BreicheSinal <sinalbreiche@gmail.com>
2024-12-13 04:08:07 +0200

    Adjusted navbar component | added css styles

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -17,42 +16,6 @@ import NoteEvent from "@mui/icons-material/EventNote";
 import Typography from "@mui/material/Typography";
 
 import "./style.css";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.black, 0.1),
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 export default function PrimarySearchAppBar() {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
@@ -140,16 +103,16 @@ export default function PrimarySearchAppBar() {
               className="logo"
             />
           </IconButton>
-          <Search>
-            <SearchIconWrapper>
+          <div className="search">
+            <div className="search-icon-wrapper">
               <SearchIcon className="search-icon" />
-            </SearchIconWrapper>
-            <StyledInputBase
+            </div>
+            <InputBase
               className="search-input"
               placeholder="Search…"
               inputProps={{ "aria-label": "search" }}
             />
-          </Search>
+          </div>
           <Box sx={{ flexGrow: 1 }} />
           <Box
             className="icons-container"
