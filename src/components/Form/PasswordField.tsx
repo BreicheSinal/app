3e946c90482b6f389bdb5c4c9573f8b9dfddb109
BreicheSinal <sinalbreiche@gmail.com>
2024-12-18 01:54:10 +0200

    Made password field a separate component

---
+++ src/components/Form/PasswordField.tsx
@@ -0,0 +1,55 @@
+import {
+  TextField,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { FC, useState, ChangeEvent } from "react";
+
+interface PasswordFieldProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const PasswordField: FC<PasswordFieldProps> = ({ value, onChange }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+  return (
+    <>
+      <InputLabel sx={{ color: "#2684ff" }}>Password</InputLabel>
+      <TextField
+        fullWidth
+        type={showPassword ? "text" : "password"}
+        value={value}
+        onChange={onChange}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                onClick={togglePasswordVisibility}
+                edge="end"
+                sx={{ color: "white" }}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        sx={{
+          input: { color: "#ffffff" },
+          backgroundColor: "#393939",
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": { borderColor: "white" },
+            "&:hover fieldset": { borderColor: "white" },
+            "&.Mui-focused fieldset": { borderColor: "white" },
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default PasswordField;
