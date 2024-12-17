4a50999db774159d69e6d67dc3b408ef22148054
BreicheSinal <sinalbreiche@gmail.com>
2024-12-17 20:09:57 +0200

    Added the layout sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,39 @@
 import { FC } from "react";
-
+import { Grid, Box } from "@mui/material";
 import "./style.css";
 
 const SignUp: FC = () => {
   return (
-    <div>
-      <div className="main-container"></div>
-    </div>
+    <Grid
+      container
+      component="main"
+      sx={{ height: "100vh", backgroundColor: "#1d2125" }}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></Grid>
+
+      <Grid
+        item
+        xs={false}
+        sm={6}
+        md={6}
+        sx={{
+          backgroundImage: "url('src/assets/images/sports.webp')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    </Grid>
   );
 };
 
