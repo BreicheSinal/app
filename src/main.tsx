3a063cdd2c7bc3c76ff03c2a4edd52ed35601fe7
BreicheSinal <sinalbreiche@gmail.com>
2024-12-23 20:46:48 +0200

    Setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 import "./ui/styles/index.css";
 
 import App from "./lib/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
