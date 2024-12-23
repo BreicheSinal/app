7d1ad9e005c32e6c1d20ccccabd520bec03bf0e1
BreicheSinal <sinalbreiche@gmail.com>
2024-12-23 23:26:35 +0200

    Updated redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import athleteReducer from "./users/athleteSlice";
+import coachReducer from "./users/coachSlice";
+import clubReducer from "./users/clubSlice";
+import federationReducer from "./users/federationSlice";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    athlete: athleteReducer,
+    coach: coachReducer,
+    club: clubReducer,
+    federation: federationReducer,
+  },
 });
 
 export default store;
