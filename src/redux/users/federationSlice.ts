3ce0f1d303f65e7fa44483ac6bc41b1708adf20c
BreicheSinal <sinalbreiche@gmail.com>
2024-12-23 23:17:47 +0200

    Added federation details interface

---
+++ src/redux/users/federationSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface FederationDetails {
+  id: number;
+  user_id: number;
+  federation_type_id: number;
+  name: string;
+  bio: string;
+  location: string;
+  country: string;
+  founded_year: number;
+}
+
+
