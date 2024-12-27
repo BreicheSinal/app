10e4f94fd6b2cad8e48e7c475f96b96c5310bd4a
BreicheSinal <sinalbreiche@gmail.com>
2024-12-27 23:55:23 +0200

    Updated edit bio && experience functions

--- src/core/utils/editDetails.ts
+++ src/core/utils/editDetails.ts
@@ -36,32 +36,10 @@ export const editExperience = async (
 ) => {
   try {
     if (!role) throw new Error("User role or ID missing");
+    const endpoint = `/${role.toLowerCase()}/editExpCert/${exp_id}`;
 
-    console.log(exp_id);
-    const endpoint =
-      role === "Athlete"
-        ? `/athlete/editExpCert/${exp_id}`
-        : `/coach/editExpCert/${exp_id}`;
-
-    console.log(experience);
     await requestApi(endpoint, "PUT", experience);
-
-    switch (role) {
-      case "Athlete":
-        dispatch(fetchAthleteDetails(id));
-        break;
-      case "Coach":
-        dispatch(fetchCoachDetails(id));
-        break;
-      case "Club":
-        dispatch(fetchClubDetails(id));
-        break;
-      case "Federation":
-        dispatch(fetchFederationDetails(id));
-        break;
-      default:
-        throw new Error("Invalid role");
-    }
+    await dispatchFetch(role, id, dispatch);
   } catch (error) {
     console.error("Error editing experience:", error);
     throw error;
@@ -71,43 +49,18 @@ export const editExperience = async (
 export const editBio = async (updatedBio: string, dispatch: AppDispatch) => {
   try {
     if (!role || !specificRoleId) throw new Error("User role or ID missing");
-
     const id = parseInt(specificRoleId);
 
-    // updating bio based on role
-    const endpoint =
-      role === "Athlete"
-        ? `/athlete/editBio/${id}`
-        : role === "Coach"
-        ? `/coach/editBio/${id}`
-        : role === "Club"
-        ? `/club/editBio/${id}`
-        : `/federation/editBio/${id}`;
-
+    const endpoint = `/${role.toLowerCase()}/editBio/${id}`;
     await requestApi(endpoint, "PUT", { bio: updatedBio });
-
-    // refetching updated details for the user
-    switch (role) {
-      case "Athlete":
-        dispatch(fetchAthleteDetails(id));
-        break;
-      case "Coach":
-        dispatch(fetchCoachDetails(id));
-        break;
-      case "Club":
-        dispatch(fetchClubDetails(id));
-        break;
-      case "Federation":
-        dispatch(fetchFederationDetails(id));
-        break;
-      default:
-        throw new Error("Invalid role");
-    }
+    await dispatchFetch(role, id, dispatch);
   } catch (error) {
     console.error("Error updating bio:", error);
+    throw error;
   }
 };
 
+/* EDIT PROFILE DATA */
 const formatFields = (fields: { [key: string]: string | number | null }) => {
   return Object.entries(fields).reduce((acc, [key, value]) => {
     if (key === "Club") acc["club_id"] = parseInt(value as string);
