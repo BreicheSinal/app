13410a3cbb288f77c1bdcc2fa514d047c935046b
BreicheSinal <sinalbreiche@gmail.com>
2024-12-29 06:43:20 +0200

    Updated feed page

--- src/ui/pages/Feed/index.tsx
+++ src/ui/pages/Feed/index.tsx
@@ -1,5 +1,77 @@
+import FeedLayout from "../../Layout/FeedLayout";
+
+import CustomCard from "../../components/CustomCard";
+import { ProfileCardView } from "../../components/ProfileCard";
+
+import { CardData } from "../../components/CustomCard";
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
+
+import { getProfileData } from "../../../core/utils/fetchDetails";
+
+import { getStoredRole, UserDetails } from "../../../core/utils/globalUtils";
+
+/* MOCK DATA */
+const staffData: CardData = {
+  title: "STAFF",
+  sections: [
+    {
+      type: "list",
+      content: [
+        {
+          name: "NAME",
+          role: "ROLE",
+          content: "Alice is responsible for implementing the backend APIs.",
+        },
+        { name: "NAME", role: "ROLE" },
+        { name: "NAME", role: "ROLE" },
+      ],
+    },
+  ],
+};
 const Feed = () => {
-  return <></>;
+  const role = getStoredRole();
+
+  const details = useSelector((state: RootState) =>
+    role === "Athlete"
+      ? state.athlete.details
+      : role === "Coach"
+      ? state.coach.details
+      : role === "Club"
+      ? state.club.details
+      : state.federation.details
+  );
+
+  /* getting data */
+  const profileData = useMemo(
+    () =>
+      details
+        ? getProfileData(role, details as UserDetails)
+        : { title: "Loading...", fields: [] },
+    [role, details]
+  );
+
+  return (
+    <div className="feed-container">
+      <FeedLayout>
+        <div className="cards-container flex">
+          <ProfileCardView width={300} data={profileData} />
+
+          <div className="sub-cards-container flex">
+            <div className="flex column"></div>
+            <CustomCard
+              width={250}
+              data={staffData}
+              showEdit={false}
+              onEdit={() => {}}
+            />
+          </div>
+        </div>
+      </FeedLayout>
+    </div>
+  );
 };
 
 export default Feed;
