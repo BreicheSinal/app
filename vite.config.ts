7f8dbdad883edb8c571f71be8554598fde96000b
BreicheSinal <sinalbreiche@gmail.com>
2025-01-03 21:01:11 +0200

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    strictPort: true
+  server: {
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      "/socket.io": {
+        target: `http://localhost:8080`,
+        changeOrigin: true,
+        ws: true,
+      },
+    },
   },
-})
+});
