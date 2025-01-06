c1c4bb0feab0a09a7a0ba78f41322837367b2e55
BreicheSinal <sinalbreiche@gmail.com>
2025-01-07 00:20:18 +0200

    Added dialog styles

---
+++ src/ui/styles/dialogStyles.ts
@@ -0,0 +1,22 @@
+export const dialogStyles = {
+  "& .MuiDialog-paper": {
+    backgroundColor: "#1D2125",
+    color: "white",
+  },
+  "& .MuiDialogTitle-root": {
+    padding: "24px",
+    paddingBottom: "16px",
+  },
+  "& .MuiDialogContent-root": {
+    padding: "24px",
+    paddingTop: 0,
+    paddingBottom: "16px",
+  },
+  "& .MuiDialogContentText-root": {
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  "& .MuiDialogActions-root": {
+    padding: "24px",
+    paddingTop: "8px",
+  },
+};
