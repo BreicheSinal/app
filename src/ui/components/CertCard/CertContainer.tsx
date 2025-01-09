bee515bef016a8f7eadcfef008624617109b081c
BreicheSinal <sinalbreiche@gmail.com>
2025-01-09 21:08:40 +0200

    Updated styling in certification container

--- src/ui/components/CertCard/CertContainer.tsx
+++ src/ui/components/CertCard/CertContainer.tsx
@@ -19,7 +19,7 @@ export const CertContainer: FC<CertContainerProps> = ({
         maxWidth: "600px",
         height: "auto",
         borderRadius: 2,
-        padding: 2,
+        padding: 1,
       }}
     >
       {children}
