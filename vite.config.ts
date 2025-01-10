53f6a68c7475f44279100fa9bd71d7e2bc8ab597
BreicheSinal <sinalbreiche@gmail.com>
2025-01-10 22:48:13 +0200

    Modified vite config for datetime picker

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "date-fns": "date-fns/esm",
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
