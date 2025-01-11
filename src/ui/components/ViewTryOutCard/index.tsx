f8b6c334d2d862a07920bcd26dcf6e69e54e7910
BreicheSinal <sinalbreiche@gmail.com>
2025-01-11 19:28:41 +0200

    Added on action apply in view try out card

--- src/ui/components/ViewTryOutCard/index.tsx
+++ src/ui/components/ViewTryOutCard/index.tsx
@@ -3,12 +3,25 @@ import { useSelector } from "react-redux";
 import { Box } from "@mui/material";
 import { RootState } from "../../../redux/store";
 import { TryoutsList } from "./TryOutList";
+import { applyToTryOut } from "../../../core/utils/addDetails";
+import { useDispatch } from "react-redux";
 
 const ViewTryouts: FC = () => {
+  const dispatch = useDispatch();
+
   const { tryOuts } = useSelector((state: RootState) => state.tryOuts);
 
-  const apply = () => {
+  const athleteId = useSelector(
+    (state: RootState) => state.athlete.details?.id
+  );
 
+  const apply = async (tryoutId: number) => {
+    try {
+      await applyToTryOut(dispatch, athleteId!, tryoutId);
+    } catch (error) {
+      // add ui error here
+      console.error("Failed to apply for tryout:", error);
+    }
   };
   return (
     <Box className="flex column" sx={{ padding: "10px" }}>
