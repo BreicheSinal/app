89cfcb4a7ec8524b5844146ed148da5a1ac38aba
BreicheSinal <sinalbreiche@gmail.com>
2025-01-11 15:05:39 +0200

    Added view try outs pages

---
+++ src/ui/pages/ViewTryOuts/index.tsx
@@ -0,0 +1,57 @@
+import { FC } from "react";
+
+import FeedLayout from "../../Layout/FeedLayout";
+
+import { ProfileCardView } from "../../components/ProfileCard";
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
+import { getStoredRole, UserDetails } from "../../../core/utils/globalUtils";
+
+import { getProfileData } from "../../../core/utils/fetchDetails";
+
+import "./style.css";
+import ViewTryouts from "../../components/ViewTryOutCard";
+
+
+const ViewTryOuts: FC = () => {
+  const role = getStoredRole();
+
+  const details = useSelector((state: RootState) =>
+    role === "Athlete"
+      ? state.athlete.details
+      : role === "Coach"
+      ? state.coach.details
+      : role === "Club"
+      ? state.club.details
+      : state.federation.details
+  );
+
+  /* getting data */
+  const profileData = useMemo(
+    () =>
+      details
+        ? getProfileData(role, details as UserDetails)
+        : { title: "Loading...", fields: [] },
+    [role, details]
+  );
+
+  return (
+    <div className="feed-container">
+      <FeedLayout>
+        <div className="cards-container flex">
+          <ProfileCardView width={300} data={profileData} showConnect={false} />
+
+          <div className="sub-cards-container flex">
+            <div className="flex column">
+              <ViewTryouts />
+            </div>
+          </div>
+        </div>
+      </FeedLayout>
+    </div>
+  );
+};
+
+export default ViewTryOuts;
