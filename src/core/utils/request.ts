4b281381ed8d17c42dd45b40c524f5e5241c2e5c
BreicheSinal <sinalbreiche@gmail.com>
2025-01-15 12:29:58 +0200

    Updated request util

--- src/core/utils/request.ts
+++ src/core/utils/request.ts
@@ -3,22 +3,29 @@ import axios, { AxiosInstance, AxiosResponse, Method } from "axios";
 
 const api: AxiosInstance = axios.create({
   baseURL: "http://localhost:8080",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export const requestApi = async (
   url: string,
   method: Method = "GET",
-  data?: object
+  data?: any
 ) => {
   try {
-    const response: AxiosResponse = await api.request({
+    const config = {
       url,
       method,
       data,
-    });
+      headers: {
+        "Content-Type":
+          data instanceof FormData ? "multipart/form-data" : "application/json",
+      },
+    };
+
+    if (data instanceof FormData) {
+      delete api.defaults.headers["Content-Type"];
+    }
+
+    const response: AxiosResponse = await api.request(config);
     return response.data;
   } catch (error: any) {
     console.error("API Request Error:", error.response?.data || error.message);
