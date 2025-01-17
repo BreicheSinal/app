1c7fcdceaeecd3cdedea8384b0cb00a2d4af9dd9
BreicheSinal <sinalbreiche@gmail.com>
2025-01-17 19:06:57 +0200

    Updated styles in email field

--- src/ui/components/Form/EmailField.tsx
+++ src/ui/components/Form/EmailField.tsx
@@ -18,24 +18,24 @@ const EmailField: FC<EmailFieldProps> = ({ value, onChange, error }) => {
         fullWidth
         value={value}
         onChange={onChange}
-        variant="outlined"
+        placeholder="Email"
         error={error}
         sx={{
-          input: { color: "#1D2125" },
-          backgroundColor: "#393939",
+          input: { color: "white" },
+          backgroundColor: "#1D2125",
           "& .MuiOutlinedInput-root": {
             "& fieldset": { borderColor: "white" },
             "&:hover fieldset": { borderColor: "white" },
             "&.Mui-focused fieldset": { borderColor: "white" },
             "& input:-webkit-autofill": {
-              "-webkit-box-shadow": "0 0 0 30px #393939 inset !important",
-              "-webkit-text-fill-color": "#1D2125 !important",
+              "-webkit-box-shadow": "0 0 0 30px #1D2125 inset !important",
+              "-webkit-text-fill-color": "#FFFFFF !important",
             },
             "& input:-webkit-autofill:hover": {
-              "-webkit-box-shadow": "0 0 0 30px #393939 inset !important",
+              "-webkit-box-shadow": "0 0 0 30px #1D2125 inset !important",
             },
             "& input:-webkit-autofill:focus": {
-              "-webkit-box-shadow": "0 0 0 30px #393939 inset !important",
+              "-webkit-box-shadow": "0 0 0 30px #1D2125 inset !important",
             },
           },
         }}
