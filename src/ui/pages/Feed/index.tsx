c60a45b043ba841907336ac01168d8e60867dba1
BreicheSinal <sinalbreiche@gmail.com>
2025-01-17 19:50:43 +0200

    Updated feed page

--- src/ui/pages/Feed/index.tsx
+++ src/ui/pages/Feed/index.tsx
@@ -2,7 +2,7 @@ import FeedLayout from "../../Layout/FeedLayout";
 
 import { ProfileCardView } from "../../components/ProfileCard";
 import PostCard from "../../components/PostCard";
-import Post from "../../components/Post";
+//import Post from "../../components/Post";
 
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
@@ -13,8 +13,8 @@ import { getProfileData } from "../../../core/utils/fetchDetails";
 import { getStoredRole, UserDetails } from "../../../core/utils/globalUtils";
 import ConnectionsCard from "../../components/ConnectionCard";
 
-const handleLike = async () => {};
-const handleComment = async () => {};
+//const handleLike = async () => {};
+//const handleComment = async () => {};
 
 const Feed = () => {
   const role = getStoredRole();
@@ -42,19 +42,24 @@ const Feed = () => {
     <div className="feed-container">
       <FeedLayout>
         <div className="cards-container flex">
-          <ProfileCardView width={300} data={profileData} showConnect={false} />
+          <ProfileCardView
+            width={300}
+            data={profileData}
+            role={role}
+            showConnect={false}
+          />
 
           <div className="sub-cards-container flex">
             <div className="flex column">
               <PostCard width={600} />
-              <Post
+              {/** <Post
                 width={600}
                 userName="Name"
                 //image="../../../../src/assets/icons/AthLink_noBG1.png"
                 description="Qorem ipsum dolor sit amet, consectetur adipiscing elit..."
                 onLike={() => handleLike()}
                 onComment={() => handleComment()}
-              />
+              />*/}
             </div>
             {details?.user_id && (
               <ConnectionsCard
