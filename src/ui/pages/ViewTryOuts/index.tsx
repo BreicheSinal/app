8fd2245fa6eb92f639e0b2ad7e24c470a424cb1d
BreicheSinal <sinalbreiche@gmail.com>
2025-01-17 19:50:27 +0200

    Updated view tryouts page

--- src/ui/pages/ViewTryOuts/index.tsx
+++ src/ui/pages/ViewTryOuts/index.tsx
@@ -42,7 +42,12 @@ const ViewTryOuts: FC = () => {
     <div className="feed-container">
       <FeedLayout>
         <div className="cards-container flex">
-          <ProfileCardView width={300} data={profileData} showConnect={false} />
+          <ProfileCardView
+            width={300}
+            data={profileData}
+            role={role}
+            showConnect={false}
+          />
 
           <div className="sub-cards-container flex">
             <div className="flex column">
